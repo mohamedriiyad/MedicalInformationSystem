f3914f0a3c11b78d0cbaad4dadcbf0c594cb98f4
3mrEmad <3mr3mad47@gmail.com>
2021-04-08 21:44:38 +0200

    amrcommit3

--- MedicalInformationSystem/Migrations/20210408162934_addhospital2.Designer.cs
+++ MedicalInformationSystem/Migrations/20210408162934_addhospital2.Designer.cs
@@ -4,14 +4,16 @@ using MedicalInformationSystem.Persistant;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MedicalInformationSystem.Migrations
 {
     [DbContext(typeof(MedicalSystemDbContext))]
-    partial class MedicalSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210408162934_addhospital2")]
+    partial class addhospital2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MedicalInformationSystem/Models/HospitalModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalInformationSystem.Models
+{
+    public class HospitalModel
+    {
+
+        
+        [Key]
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+    
+
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+       
+    }
+}
